2b9bfa2390766e3f847b4501b144cad8fa8d449b
yliu <me@test.com>
2022-10-05 15:05:32 -0400

    PSI func v3.2

--- PSI/setup.py
+++ PSI/setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 
 setup(
     name='PSI',
-    version=3.1,
+    version=3.2,
     url='https://github.com/yliuuniv',
-    description='This is a Population Stability Index(PSI) Function to compare two datasets.',
+    description='This is a Population Stability Index(PSI) and Character Stability Function to compare two datasets.',
     long_description=open('README.md').read(),
     author = 'yue.liu',
     author_email = 'yueliu.univ@gmail.com',
